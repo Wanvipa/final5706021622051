4b5dfed8cabe4268eca3cb20bc779a7f3887877a
Wanvipa <kimonogirl_pa_41@hotmail.com>
2016-12-11 11:29:44 +0700

    weather

--- server.js
+++ server.js
@@ -56,21 +56,21 @@ function receivedMessage(event) {
   if (messageText) {
     if (messageText === 'hello') {
       sendTextMessage(senderID, "ฟรุ้งฟริ้ง");
-    }else if (messageText) {
-      var location = event.message.text
-          var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=7bb0ec281912240aaa2b0a632fe3f779'
-          request({
-            url: weatherEndpoint,
-            json: true
-          }, function(error, response, body) {
-            try {
-              var condition = body.main;
-              sendTextMessage(sender, "วันนี้ อุณหภูมิ " + condition.temp + " °C  " + "ความชื้น " + condition.humidity + " % ที่" + location);
-            } catch(err) {
-              console.error('error caught', err);
-              sendTextMessage(sender, "โปรดใส่ชื่อเมืองให้ถูกต้อง(ยกตัวอย่างเช่น ฺBangkok )");
-            }
-          })
+    // }else if (messageText) {
+    //   var location = event.message.text
+    //       var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=7bb0ec281912240aaa2b0a632fe3f779'
+    //       request({
+    //         url: weatherEndpoint,
+    //         json: true
+    //       }, function(error, response, body) {
+    //         try {
+    //           var condition = body.main;
+    //           sendTextMessage(sender, "วันนี้ อุณหภูมิ " + condition.temp + " °C  " + "ความชื้น " + condition.humidity + " % ที่" + location);
+    //         } catch(err) {
+    //           console.error('error caught', err);
+    //           sendTextMessage(sender, "โปรดใส่ชื่อเมืองให้ถูกต้อง(ยกตัวอย่างเช่น ฺBangkok )");
+    //         }
+    //       })
  }
 
     switch (messageText) {
